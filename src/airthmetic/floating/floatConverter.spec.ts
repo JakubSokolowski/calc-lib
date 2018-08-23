652fcbeedd27ea503f7bf68871f6b61336118567
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2018-08-23 18:41:45 +0200

    Fix the circular dependency

--- src/airthmetic/floating/floatConverter.spec.ts
+++ src/airthmetic/floating/floatConverter.spec.ts
@@ -1,5 +1,84 @@
-import { FloatProperty } from './floatRepresentation'
-import { FloatConverter } from './floatConverter'
+import {
+  DoubleRepresentation,
+  FloatConverter,
+  FloatProperty,
+  SingleRepresentation
+} from './floatConverter'
+
+describe('SingleRepresentation constructor test', () => {
+  it('constructs valid single representation from binary string', () => {
+    let input = '01000000010010001111010111000011'
+    let rep = new SingleRepresentation(input)
+    expect(rep.sign).toEqual('0')
+    expect(rep.exponent).toEqual('10000000')
+    expect(rep.mantissa).toEqual('10010001111010111000011')
+  })
+})
+describe('SingleRepresentation field encoding tests', () => {
+  it('returns correct encodings for exponent and mantissa', () => {
+    let input = '01000000010010001111010111000011'
+    let rep = new SingleRepresentation(input)
+    expect(rep.exponentEncoding).toEqual(128)
+    expect(rep.mantissaEncoding).toEqual(4781507)
+  })
+})
+describe('SingleRepresentation field value tests', () => {
+  it('returns correct values for exponent and mantissa', () => {
+    let input = '01000000010010001111010111000011'
+    let rep = new SingleRepresentation(input)
+    expect(rep.exponentValue).toEqual(1)
+    expect(rep.mantissaValue).toEqual(1.5700000524520874)
+  })
+})
+describe('SingleRepresentation binary str conversion tests', () => {
+  it('returns correct values for exponent and mantissa', () => {
+    let input = '01000000010010001111010111000011'
+    let rep = new SingleRepresentation(input)
+    let expectedFixed = '3.14'
+    let expectedFixedMaxPrecision = '3.1400001049041748046875'
+    expect(rep.value.toFixed(2)).toEqual(expectedFixed)
+    expect(rep.value.toFixed(22)).toEqual(expectedFixedMaxPrecision)
+  })
+})
+describe('DoubleRepresentation constructor test', () => {
+  it('constructs valid single representation from binary string', () => {
+    let input = '0100000000001001000111101011100001010001111010111000010100011111\n'
+    let rep = new DoubleRepresentation(input)
+    expect(rep.sign).toEqual('0')
+    expect(rep.exponent).toEqual('10000000000')
+    expect(rep.mantissa).toEqual('1001000111101011100001010001111010111000010100011111')
+  })
+})
+describe('DoubleRepresentation field encoding tests', () => {
+  it('returns correct encodings for exponent and mantissa', () => {
+    let input = '0100000000001001000111101011100001010001111010111000010100011111'
+    let rep = new DoubleRepresentation(input)
+    expect(rep.exponentEncoding).toEqual(1024)
+    expect(rep.mantissaEncoding).toEqual(2567051787601183)
+  })
+})
+describe('DoubleRepresentation field value tests', () => {
+  it('returns correct values for exponent and mantissa', () => {
+    let input = '0100000000001001000111101011100001010001111010111000010100011111'
+    let rep = new DoubleRepresentation(input)
+    expect(rep.exponentValue).toEqual(1)
+    expect(rep.mantissaValue).toEqual(1.57)
+    expect(rep.mantissaValue.toFixed(52)).toEqual(
+      '1.5700000000000000621724893790087662637233734130859375'
+    )
+  })
+
+  describe('DoubleRepresentation binary str conversion tests', () => {
+    it('returns correct values for exponent and mantissa', () => {
+      let input = '0100000000001001000111101011100001010001111010111000010100011111'
+      let rep = new DoubleRepresentation(input)
+      expect(rep.value.toFixed(2)).toEqual('3.14')
+      expect(rep.value.toFixed(52)).toEqual(
+        '3.1400000000000001243449787580175325274467468261718750'
+      )
+    })
+  })
+})
 
 describe('ToSingle tests', () => {
   it('returns representation that matches binary string', () => {
--- src/airthmetic/floating/floatConverter.ts
+++ src/airthmetic/floating/floatConverter.ts
@@ -1,10 +1,89 @@
-import {
-  DoubleRepresentation,
-  FloatingRepresentation,
-  FloatProperty,
-  SingleRepresentation
-} from './floatRepresentation'
 import { prependZeros } from '../conversionHelpers'
+import { BaseConverter } from '../positional/baseConverter'
+
+export enum FloatProperty {
+  Normalized,
+  Denormalized,
+  PositiveInfinity,
+  NegativeInfinity,
+  PositiveZero,
+  NegativeZero,
+  NAN
+}
+
+export abstract class FloatingRepresentation {
+  exponentLength: number = 8
+  mantissaLength: number = 23
+  binaryLength: number = 32
+  bias: number = 127
+  binary: string = ''
+
+  abstract get value(): number
+
+  abstract get sign(): string
+
+  abstract get exponent(): string
+
+  abstract get mantissa(): string
+
+  abstract get exponentEncoding(): number
+
+  abstract get mantissaEncoding(): number
+
+  abstract get exponentValue(): number
+
+  abstract get mantissaValue(): number
+}
+
+export class SingleRepresentation extends FloatingRepresentation {
+  constructor(str: string) {
+    super()
+    this.binary = str
+  }
+
+  get value() {
+    return FloatConverter.BinaryStringToSingle(this.binary)
+  }
+
+  get sign() {
+    return this.binary[0]
+  }
+
+  get exponent() {
+    return this.binary.substr(1, this.exponentLength)
+  }
+
+  get mantissa() {
+    return this.binary.substr(1 + this.exponentLength, this.mantissaLength)
+  }
+
+  get exponentEncoding() {
+    return Number.parseInt(this.exponent, 2)
+  }
+
+  get mantissaEncoding() {
+    return Number.parseInt('0' + this.mantissa, 2)
+  }
+
+  get exponentValue() {
+    return this.exponentEncoding - this.bias
+  }
+
+  get mantissaValue() {
+    return BaseConverter.fromValueString('1.' + this.mantissa, 2, 10).valueInDecimal.toNumber()
+  }
+}
+
+export class DoubleRepresentation extends SingleRepresentation {
+  exponentLength = 11
+  mantissaLength = 52
+  binaryLength = 64
+  bias = 1023
+
+  get value() {
+    return FloatConverter.BinaryStringToDouble(this.binary)
+  }
+}
 
 export class FloatConverter {
   static ToSingle(val: number | string): SingleRepresentation {
@@ -32,16 +111,8 @@ export class FloatConverter {
     let doubleView = new Float64Array(buffer)
     let intView = new Int32Array(buffer)
     doubleView[0] = d
-    // let low = intView[0].toString(2);
-    // let high = intView[1].toString(2);
     let low = (intView[0] >>> 0).toString(2)
     let high = (intView[1] >>> 0).toString(2)
-    // if(high.startsWith('-')) {
-    //   high = high.substring(1);
-    //   high = prependStr('1', high, 32);
-    // } else {
-    //
-    // }
     low = prependZeros(low, 32)
     high = prependZeros(high, 32)
     return high + low
