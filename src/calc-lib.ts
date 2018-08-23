a513297033a34683c51f44dd5af6402a0b0816e5
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2018-08-23 18:43:54 +0200

    Fix imports

--- src/calc-lib.ts
+++ src/calc-lib.ts
@@ -1,11 +1,11 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-export { BaseConverter } from './airthmetic/positional/baseConverter'
-export { BaseRepresentation } from './airthmetic/positional/baseConverter'
-export { ComplementConverter } from './airthmetic/positional/complementConverter'
-export { BaseComplement } from './airthmetic/positional/complementConverter'
-export { FloatConverter } from './airthmetic/floating/floatConverter'
-export { FloatingRepresentation } from './airthmetic/floating/floatRepresentation'
-export { SingleRepresentation } from './airthmetic/floating/floatRepresentation'
-export { DoubleRepresentation } from './airthmetic/floating/floatRepresentation'
+export { BaseConverter, BaseRepresentation } from './airthmetic/positional/baseConverter'
+export { ComplementConverter, BaseComplement } from './airthmetic/positional/complementConverter'
+export {
+  FloatConverter,
+  FloatingRepresentation,
+  SingleRepresentation,
+  DoubleRepresentation
+} from './airthmetic/floating/floatConverter'
