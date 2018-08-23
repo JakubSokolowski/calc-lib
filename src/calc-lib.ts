4181564930a7ae9a884014e3296f3aa6c554e909
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2018-08-23 18:35:40 +0200

    Remove dummy export. Expose classes from lib.

--- src/calc-lib.ts
+++ src/calc-lib.ts
@@ -1,4 +1,11 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-export default class DummyClass {}
+export { BaseConverter } from './airthmetic/positional/baseConverter'
+export { BaseRepresentation } from './airthmetic/positional/baseConverter'
+export { ComplementConverter } from './airthmetic/positional/complementConverter'
+export { BaseComplement } from './airthmetic/positional/complementConverter'
+export { FloatConverter } from './airthmetic/floating/floatConverter'
+export { FloatingRepresentation } from './airthmetic/floating/floatRepresentation'
+export { SingleRepresentation } from './airthmetic/floating/floatRepresentation'
+export { DoubleRepresentation } from './airthmetic/floating/floatRepresentation'
