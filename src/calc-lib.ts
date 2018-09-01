0a2b22d7712a683f4d7fd6754e602694066f7627
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2018-09-01 22:40:19 +0200

    Update version to 0.0.2. Change exports to reflect changes

--- src/calc-lib.ts
+++ src/calc-lib.ts
@@ -1,7 +1,12 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-export { BaseConverter, BaseRepresentation } from './airthmetic/positional/baseConverter'
+export {
+  BaseRepresentation,
+  StandardBaseConverter,
+  fromNumber,
+  fromString
+} from './airthmetic/positional/baseConverter'
 export { ComplementConverter, BaseComplement } from './airthmetic/positional/complementConverter'
 export {
   FloatConverter,
